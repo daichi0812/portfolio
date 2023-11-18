c0445622b0583d98db29dfe50915dfb11bca6935
daichi0812 <ijrtrksx16.vtbvd@4rqzh@gmail.com>
2023-11-19 00:53:04 +0900

    first commit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const HomePage = () => {
+  return (
+    <div>
+        <Button>ボタン</Button>
+    </div>
+  )
+}
+
+export default HomePage
