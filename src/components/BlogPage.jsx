7e0da4ba3e1d62b6c1931f36ea9cb16945004595
daichi0812 <ijrtrksx16.vtbvd@4rqzh@gmail.com>
2023-11-19 20:17:05 +0900

    簡易ポートフォリオ

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -1,9 +1,73 @@
 import React from 'react'
 
 const BlogPage = () => {
-  return (
-    <div>BlogPage</div>
-  )
+    return (
+        <div className="container">
+            <h1>ブログ</h1>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+                sequi dolorem deleniti harum accusamus, debitis adipisci minima
+                odit tempore, voluptas mollitia. At nam dolore quisquam iure prov
+                ident aut optio cum perspiciatis id omnis. Iusto, quas. Earum, iure invent
+                ore! Voluptatem aspernatur ipsum pariatur nisi perferendis enim alias
+                debitis. Nulla facilis voluptates unde provident et deserunt quidem 
+                iure adipisci libero atque officia vero, magni perspiciatis optio harum.
+                Obcaecati soluta repellat dolore! Accusamus alias nobis harum cumque exercitationem est unde veniam?
+                Sint aperiam consectetur totam at minus veritatis accusantium, quidem, 
+                nesciunt quam nostrum impedit quis suscipit aspernatur perferendis. Quasi vel molestiae dolorem quas.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+                sequi dolorem deleniti harum accusamus, debitis adipisci minima
+                odit tempore, voluptas mollitia. At nam dolore quisquam iure prov
+                ident aut optio cum perspiciatis id omnis. Iusto, quas. Earum, iure invent
+                ore! Voluptatem aspernatur ipsum pariatur nisi perferendis enim alias
+                debitis. Nulla facilis voluptates unde provident et deserunt quidem 
+                iure adipisci libero atque officia vero, magni perspiciatis optio harum.
+                Obcaecati soluta repellat dolore! Accusamus alias nobis harum cumque exercitationem est unde veniam?
+                Sint aperiam consectetur totam at minus veritatis accusantium, quidem, 
+                nesciunt quam nostrum impedit quis suscipit aspernatur perferendis. Quasi vel molestiae dolorem quas.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+                sequi dolorem deleniti harum accusamus, debitis adipisci minima
+                odit tempore, voluptas mollitia. At nam dolore quisquam iure prov
+                ident aut optio cum perspiciatis id omnis. Iusto, quas. Earum, iure invent
+                ore! Voluptatem aspernatur ipsum pariatur nisi perferendis enim alias
+                debitis. Nulla facilis voluptates unde provident et deserunt quidem 
+                iure adipisci libero atque officia vero, magni perspiciatis optio harum.
+                Obcaecati soluta repellat dolore! Accusamus alias nobis harum cumque exercitationem est unde veniam?
+                Sint aperiam consectetur totam at minus veritatis accusantium, quidem, 
+                nesciunt quam nostrum impedit quis suscipit aspernatur perferendis. Quasi vel molestiae dolorem quas.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+                sequi dolorem deleniti harum accusamus, debitis adipisci minima
+                odit tempore, voluptas mollitia. At nam dolore quisquam iure prov
+                ident aut optio cum perspiciatis id omnis. Iusto, quas. Earum, iure invent
+                ore! Voluptatem aspernatur ipsum pariatur nisi perferendis enim alias
+                debitis. Nulla facilis voluptates unde provident et deserunt quidem 
+                iure adipisci libero atque officia vero, magni perspiciatis optio harum.
+                Obcaecati soluta repellat dolore! Accusamus alias nobis harum cumque exercitationem est unde veniam?
+                Sint aperiam consectetur totam at minus veritatis accusantium, quidem, 
+                nesciunt quam nostrum impedit quis suscipit aspernatur perferendis. Quasi vel molestiae dolorem quas.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+                sequi dolorem deleniti harum accusamus, debitis adipisci minima
+                odit tempore, voluptas mollitia. At nam dolore quisquam iure prov
+                ident aut optio cum perspiciatis id omnis. Iusto, quas. Earum, iure invent
+                ore! Voluptatem aspernatur ipsum pariatur nisi perferendis enim alias
+                debitis. Nulla facilis voluptates unde provident et deserunt quidem 
+                iure adipisci libero atque officia vero, magni perspiciatis optio harum.
+                Obcaecati soluta repellat dolore! Accusamus alias nobis harum cumque exercitationem est unde veniam?
+                Sint aperiam consectetur totam at minus veritatis accusantium, quidem, 
+                nesciunt quam nostrum impedit quis suscipit aspernatur perferendis. Quasi vel molestiae dolorem quas.
+            </p>
+
+        </div>
+    )
 }
 
 export default BlogPage
