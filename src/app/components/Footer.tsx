8b7c028aaad59bbc6a3482b147c96ab36d569d60
a5822108 <a5822108@aoyama.jp>
2024-10-10 15:45:22 +0900

    6: footerの調整

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className='footer z-10 border border-t-[#33353F] border-l-transparent border-r-transparent text-white'>
         <div className='container p-12 flex justify-between'>
             <span>
-                LOGO
+                Portfolio
             </span>
             <p className='text-slate-600'>
                 All rights reserved.
